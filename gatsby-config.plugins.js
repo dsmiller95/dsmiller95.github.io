285fed5e256d48c11ca2830506f38f1873d61914
Dan M <dsmiller95@gmail.com>
2024-05-04 13:49:23 -0500

    add header links

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -49,7 +49,8 @@ module.exports = [
             },
           },
           'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files'
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-autolink-headers'
         ]
       }
     },
